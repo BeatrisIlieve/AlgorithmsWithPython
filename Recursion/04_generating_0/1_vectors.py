004cc1fc9dca387a318754d1d05e8108ac3bf04c
Beatris Ilieve <beatrisilieve@icloud.com>
2023-11-29 14:57:19 +0200

    Added solution

---
+++ Recursion/04_generating_0/1_vectors.py
@@ -0,0 +1,31 @@
+"""
+Problem description:
+    Generate all n-bit vectors of 0 and 1 in lexicographic order.
+"""
+
+
+def generate_01(index, vector):
+    if index >= len(vector):
+        print(*vector, sep='')
+        return
+
+    for num in range(2):
+        vector[index] = num
+        generate_01(index + 1, vector)
+
+
+n = 3
+vector = [None] * n
+generate_01(0, vector)
+
+"""
+Result: 
+    000
+    001
+    010
+    011
+    100
+    101
+    110
+    111
+"""

83df80d81e43f4fdde62fe073685676eaba483bc
Beatris Ilieve <beatrisilieve@icloud.com>
2023-11-29 12:41:22 +0200

    Added solution

---
+++ Recursion/01_recursive_array_sum.py
@@ -0,0 +1,17 @@
+"""
+Problem description:
+    Write a program that finds the sum of all elements in an integer array. Use recursion.
+    Note: In practice, this recursion should not be used here (instead use an iterative solution)
+"""
+
+
+def calculate_sum(numbers, index):
+    if index == len(numbers) - 1:
+        return numbers[index]
+
+    return numbers[index] + calculate_sum(numbers, index + 1)
+
+
+list_of_numbers = [1, 2, 3, 4]
+start_index = 0
+print(calculate_sum(list_of_numbers, start_index))
